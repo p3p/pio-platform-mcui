4e7620ea9aaa8c9c313d696ae67598358f520a62
Christopher Pepper <p3p@p3psoft.co.uk>
2024-05-26 17:14:06 +0100

    plug in the default usart isrs

--- mcu_interface/src/device/LPC408x7x/interface/uart.cpp
+++ mcu_interface/src/device/LPC408x7x/interface/uart.cpp
@@ -7,4 +7,20 @@ extern "C" void UART0_IRQHandler() {
   BufferedUARTC::call_isr(0);
 }
 
+extern "C" void UART1_IRQHandler() {
+  BufferedUARTC::call_isr(1);
+}
+
+extern "C" void UART2_IRQHandler() {
+  BufferedUARTC::call_isr(2);
+}
+
+extern "C" void UART3_IRQHandler() {
+  BufferedUARTC::call_isr(3);
+}
+
+extern "C" void UART4_IRQHandler() {
+  BufferedUARTC::call_isr(4);
+}
+
 }
