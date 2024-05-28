bd46d0ce299c02a2f895cfacd42d2488779d0562
Christopher Pepper <p3p@p3psoft.co.uk>
2024-05-28 21:06:17 +0100

    simplify iap part id lookup

--- mcu_interface/src/device/LPC408x7x/interface/iap.h
+++ mcu_interface/src/device/LPC408x7x/interface/iap.h
@@ -8,6 +8,16 @@ namespace LPC4078 {
 constexpr uintptr_t iap_location = 0x1FFF1FF1;
 typedef void (*IAPEntry)(uint32_t*, uint32_t*);
 static IAPEntry iap_entry = reinterpret_cast<IAPEntry>(iap_location);
+constexpr const char* iap_part_id_string[] = {"UNKNOWN", "LPC4088", "LPC4078", "LPC4076", "LPC4074"};
+constexpr inline const char* iap_part_id_lookup(uint32_t id) {
+  switch(id) {
+    case 0x481D3F47: return iap_part_id_string[1];
+    case 0x47193F47: return iap_part_id_string[2];
+    case 0x47191F43: return iap_part_id_string[3];
+    case 0x47011132: return iap_part_id_string[4];
+  }
+  return iap_part_id_string[0];
+}
 
 struct IAP {
   enum class CommandID : uint32_t {
@@ -61,6 +71,16 @@ struct IAP {
     MCUCore::nvic_interrupts_enable();
     return result;
   };
+
+  struct PartID {
+    const char* name;
+    uint32_t id;
+  };
+
+  static inline PartID device_part_id() {
+    auto res = execute(CommandID::ReadPartID);
+    return { iap_part_id_lookup(res.value[0]), res.value[0] };
+  }
 
   static inline CommandResult device_serial_number() {
     return execute(CommandID::ReadSerialNumber);
